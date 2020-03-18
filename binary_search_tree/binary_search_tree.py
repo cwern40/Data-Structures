4e77f27ce70b13798bbd0f3bc10d43d253d4a046
cwern40 <chris.wernli@hotmail.com>
2020-03-17 19:23:31 -0600

    I finished the insert, contains, and get_max functions and am workingo on the for_each function

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+# from dll_queue import Queue
+# from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -13,6 +13,9 @@ class BinarySearchTree:
     # Insert the given value into the tree
     def insert(self, value):
         if value == self.value:
+            change = self.right
+            self.right = BinarySearchTree(value)
+            self.right.right = change
             return
         if value > self.value:
             if self.right == None:
@@ -28,16 +31,33 @@ class BinarySearchTree:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        if target > self.value:
+            if self.right == None:
+                return False
+            else:
+                return self.right.contains(target)
+        if target < self.value:
+            if self.left == None:
+                return False
+            else:
+                return self.left.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right == None:
+            return self.value
+        else:
+            return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        if self.right != None:
+            return self.right.for_each(cb)
+        if self.left != None:
+            return self.left.for_each(cb)
 
     # DAY 2 Project -----------------------
 
